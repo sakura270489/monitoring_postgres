4552bd7fdf1d8869216b6f1b010a9b4125db6cf4
sari apriliani <sariapriliani@gmail.com>
2020-06-02 17:10:47 +0700

    perbaikan isi form

--- monitoring_db_sql.php
+++ monitoring_db_sql.php
@@ -1,3 +1,9 @@
+<script type = "text/JavaScript">
+            function AutoRefresh( t ) {
+               setTimeout("location.reload(true);", t);
+            }
+      </script>
+
 <?php
 // $page = $_SERVER['PHP_SELF'];
 // $sec = "2";
@@ -6,18 +12,97 @@
 
 $servername = "172.18.0.85";
 // $database = "u266072517_name";
-$username = "root";
-$password = "adminkpde2007";
+$username = "monitoring_sql";
+$password = "monitoring_sql_2020";
 // Create connection
 $conn = mysqli_connect($servername, $username, $password);
 // Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-echo "Connected successfully";
-mysqli_close($conn);
+// echo "Connected successfully";
+
  
 	
 ?>
 
-lalalala
+<meta http-equiv="refresh" content="<?php echo $sec?>;URL='<?php echo $page?>'">
+<body onload = "JavaScript:AutoRefresh(2000);">
+	<form name="form1" method="post" id="form1">
+	<div class="breadcrumbs ace-save-state" id="breadcrumbs">
+						<ul class="breadcrumb">
+							<li>
+								<i class="ace-icon fa fa-home home-icon"></i>
+								<a href="home.php?halaman=selamat_datang">Home</a>
+							</li>
+
+							<li>
+								<a href="#">SSW</a>
+							</li>
+							<li class="active">Monitoring Aktifitas</li>
+						</ul><!-- /.breadcrumb -->
+	</div>
+	<div class="page-header">
+		<h1>
+			Monitoring DB SQL 85
+		</h1>
+	</div>
+	<table border=0 align="right">
+		<tr>
+			<td><h3><strong>Hapus semua jika sudah lebih dari 30 menit </strong></h3></td>
+			<td>&nbsp;</td>
+			<td>&nbsp;</td>
+			<td><a href="#" class="btn btn-app btn-success" onClick="document.location.reload(true)">Reload</a>&nbsp;&nbsp;&nbsp;<a href="ssw_hapus_semua.php?hapus=ssw" class="btn btn-app btn-danger btn-sm"><i class="ace-icon fa fa-trash-o bigger-200"></i> Hapus</a></td>
+		</tr>
+	</table>
+	<p>&nbsp;</p>
+	<table border=1 id="simple-table" class="table  table-bordered table-hover">
+	
+		<tr>
+			<th class="detail-col">PID</th>
+			<th class="detail-col">Username</th>
+			<th class="detail-col">Host Client</th>
+			<th class="detail-col">Database</th>
+			<th class="detail-col">Command </th>
+			<th class="detail-col">Time </th>
+			<th class="detail-col">State</th>
+			<th class="detail-col">Info</th>
+			<th class="detail-col">Action</th>
+		</tr>
+		
+
+<?php	
+	// $df = "select pid, usename, application_name, client_addr, backend_start, query_start, wait_event_type, query, datname, state_change, (EXTRACT(milliseconds FROM state_change - backend_start)) as time_per_milliseconds from pg_stat_activity where query != ''";
+	$df = "select * from information_schema.processlist order by db asc";
+	// echo $df."<br>";
+	$result = mysqli_query($conn, $df);
+	while($output = mysqli_fetch_array($result)){
+		// echo $output[0];
+?>
+<?php
+	$output[7] = mysqli_real_escape_string($output['INFO']);
+?>
+		<tr>
+			<td><?php echo $output['ID'];?></td>
+			<td><?php echo $output['USER'];?></td>
+			<td><?php echo $output['HOST'];?></td>
+			<td><?php echo $output['DB'];?></td>
+			<td><?php echo $output['COMMAND'];?></td>
+			<td><?php echo $output['TIME'];?></td>
+			<td><?php echo $output['STATE'];?></td>
+			<td><?php echo $output[7];?></td>
+			
+		</tr>
+	
+
+<?php
+		
+	}
+	
+    // pg_close($db_connection);
+    mysqli_close($conn);
+
+?>	
+
+</table>
+</form>
